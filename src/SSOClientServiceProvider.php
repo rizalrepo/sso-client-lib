b3ecaa26992c2bea645c01f7ef6214716e3004b0
rizalrepo <rizalyusei@gmail.com>
2024-04-03 13:29:01 +0800

    fix

--- src/SSOClientServiceProvider.php
+++ src/SSOClientServiceProvider.php
@@ -9,13 +9,9 @@ class SSOClientServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/routes.php');
-        // $this->publishes([
-        //     __DIR__ . '/SSOController.php' => $this->getControllerPath('SSOController.php'),
-        // ], 'sso-controller');
-
         $this->publishes([
-            __DIR__ . '/SSOController.php' => app_path('Http/Controllers/SSOController.php'),
-        ]);
+            __DIR__ . '/SSOController.php' => $this->getControllerPath('SSOController.php'),
+        ], 'sso-controller');
     }
 
     public function register()
@@ -23,8 +19,8 @@ class SSOClientServiceProvider extends ServiceProvider
         //
     }
 
-    // protected function getControllerPath($fileName)
-    // {
-    //     return realpath(__DIR__ . '/../../app/Http/Controllers/SSO/') . '/' . $fileName;
-    // }
+    protected function getControllerPath($fileName)
+    {
+        return realpath(__DIR__ . '/../../app/Http/Controllers/') . '/' . $fileName;
+    }
 }
