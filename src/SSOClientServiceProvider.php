5adb7d60925abeb3d7bfb3dc7c3dc4b3cde1c74a
rizalrepo <rizalyusei@gmail.com>
2024-04-03 14:29:07 +0800

    bismillah fix

--- src/SSOClientServiceProvider.php
+++ src/SSOClientServiceProvider.php
@@ -8,9 +8,6 @@ class SSOClientServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/sso.php', 'sso');
-        $this->loadRoutesFrom(__DIR__ . '/../config/sso.php', 'web');
-
         $this->publishes([
             __DIR__ . '/SSOController.php' => app_path('Http/Controllers/SSO/SSOController.php'),
         ], 'sso-controller');
