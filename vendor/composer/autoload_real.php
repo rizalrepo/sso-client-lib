bcfb19414c2b6ec243774a3be53692d4aea48e8e
rizalrepo <rizalyusei@gmail.com>
2024-04-04 12:35:57 +0800

    bismillah

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,6 @@ class ComposerAutoloaderInite0c57f9a2d6ae60a8710f4b9e64608b2
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInite0c57f9a2d6ae60a8710f4b9e64608b2::$files;
-        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        }, null, null);
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            $requireFile($fileIdentifier, $file);
-        }
-
         return $loader;
     }
 }
