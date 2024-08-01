5263c27812a96fd27a0487c56bc53033269d5a33
rizalrepo <rizalyusei@gmail.com>
2024-08-01 11:35:41 +0800

    Fix path

--- src/SSOClientServiceProvider.php
+++ src/SSOClientServiceProvider.php
@@ -3,8 +3,6 @@
 namespace rizalrepo\SsoClient;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Config;
 
 class SSOClientServiceProvider extends ServiceProvider
 {
@@ -16,8 +14,8 @@ class SSOClientServiceProvider extends ServiceProvider
     protected function publishConfig()
     {
         $this->publishes([
-            __DIR__ . '/SSOController.php' => App::path('Http/Controllers/SSO/SSOController.php'),
-            __DIR__ . '/sso.php' => Config::path('sso.php'),
+            __DIR__ . '/SSOController.php' => app_path('Http/Controllers/SSO/SSOController.php'),
+            __DIR__ . '/sso.php' => config_path('sso.php'),
         ], 'sso-config');
     }
 
