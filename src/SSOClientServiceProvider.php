4cb81da1c8d3e634853a574c8ce389ec36c74a8d
rizalrepo <rizalyusei@gmail.com>
2024-08-01 11:39:50 +0800

    Fix publish again

--- src/SSOClientServiceProvider.php
+++ src/SSOClientServiceProvider.php
@@ -2,25 +2,29 @@
 
 namespace rizalrepo\SsoClient;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class SSOClientServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishControllers();
         $this->publishConfig();
+    }
+
+    protected function publishControllers()
+    {
+        $this->publishes([
+            __DIR__ . '/SSOController.php' => App::path('Http/Controllers/SSO/SSOController.php'),
+        ], 'sso-config');
     }
 
     protected function publishConfig()
     {
         $this->publishes([
-            __DIR__ . '/SSOController.php' => app_path('Http/Controllers/SSO/SSOController.php'),
-            __DIR__ . '/sso.php' => config_path('sso.php'),
+            __DIR__ . '/sso.php' => Config::path('sso.php'),
         ], 'sso-config');
-    }
-
-    public function register()
-    {
-        //
     }
 }
